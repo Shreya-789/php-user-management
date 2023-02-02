27b2e5844d02a4518e4a489c51285f1107461934
Shreya-789 <124235832+Shreya-789@users.noreply.github.com>
2023-02-02 15:38:22 +0530

    Frontend changes

--- login.php
+++ login.php
@@ -4,90 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css"/>
     <title>PHP user management!</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Php Login System</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="register.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            
-            <li class="nav-item">
-              <a class="nav-link" href="logout.php">Logout</a>
-            </li>
-      
-            
-           
-          </ul>
-        </div>
-      </nav>
-      
-      <div class="container mt-4">
-      <h3>Please Login Here:</h3>
-      <hr>
-      <form action="" method="post">
-      
-        <div class="form-group">
-          <label for="inputFirstname">First name</label>
-          <input type="text" class="form-control" id="inputFirstname" placeholder="First name">
-        </div>
-        <div class="form-group">
-          <label for="inputLastname">Last name</label>
-          <input type="text" class="form-control" id="inputlastname" placeholder="Last name">
-        </div>
-        <div class="form-group">
-          <label for="inputDOB">D.O.B</label>
-          <input type="date" class="form-control" id="inputDOB" placeholder="DOB">
-        </div>
-        <div class="form-group">
-          <label for="inputEmail">Email</label>
-          <input type="email" class="form-control" id="inputEmail" placeholder="Email">
-        </div>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="inputcontactnumber">Contact Number</label>
-            <input type="tel" class="form-control" id="inputContactnumber">
-          </div>
-        </div>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="inputEmail4">Username</label>
-            <input type="text" class="form-control" name="username" id="inputEmail4" placeholder="Email">
-          </div>
-          <div class="form-group col-md-6">
-            <label for="inputPassword4">Password</label>
-            <input type="password" class="form-control" name ="password" id="inputPassword4" placeholder="Password">
-          </div>
-        </div>
-        <div class="form-group">
-            <label for="inputPassword4">Confirm Password</label>
-            <input type="password" class="form-control" name ="confirm_password" id="inputPassword" placeholder="Confirm Password">
-          </div>
-        
-        <div class="form-group">
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="gridCheck">
-            <label class="form-check-label" for="gridCheck">
-              Check me out
-            </label>
-          </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Sign in</button>
-      </form>
-      </div>
-      
-          <!-- Optional JavaScript -->
-          <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    
+  <form class="form" method="post" name="login">
+        <h1 class="login-title">Login</h1>
+        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
+        <input type="password" class="login-input" name="password" placeholder="Password"/>
+        <input type="submit" value="Login" name="submit" class="login-button"/>
+        <p class="link"><a href="register.php">New Registration</a></p>
+  </form>
 </body>
 </html>
